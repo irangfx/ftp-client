a4b1f87df0ce250a3ae6a9f02f2d80bf04d77e99
HamidNE <hamidne@mail.ru>
2019-05-31 23:21:06 +0430

    uncomment handles

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,9 +24,9 @@ class HomeController extends Controller
         $this->init();
 
         $list = $this->getFilesList();
-//        $this->downloadFiles($list);
-//        $this->prepareFiles($list);
-//        $this->uploadFiles($list);
+        $this->downloadFiles($list);
+        $this->prepareFiles($list);
+        $this->uploadFiles($list);
 
         return $list;
     }
