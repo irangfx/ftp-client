51d5e0806027b6e234f61d10065e18f332d70d5d
HamidNE <hamidne@mail.ru>
2019-06-01 00:59:37 +0430

    edit for PrepareArchiveJob.php and DownloadFileFromFTPJob.php jobs

--- app/Jobs/PrepareArchiveJob.php
+++ app/Jobs/PrepareArchiveJob.php
@@ -7,19 +7,36 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
 
 class PrepareArchiveJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    /**
+     * @var string
+     */
+    private $oldName;
+    /**
+     * @var string
+     */
+    private $newName;
+    /**
+     * @var string
+     */
+    private $fileName;
 
     /**
      * Create a new job instance.
      *
-     * @return void
+     * @param string $fileName
      */
-    public function __construct()
+    public function __construct(string $fileName)
     {
-        //
+        $this->fileName = $fileName;
+
+        $this->oldName = basename($fileName);
+        $this->newName = str_replace('tarhan.ir', 'irangfx.com', $this->oldName);;
     }
 
     /**
@@ -29,6 +46,11 @@ class PrepareArchiveJob implements ShouldQueue
      */
     public function handle()
     {
-        //
+        $storagePath = storage_path('app/tmp');
+        $command = "cd {$storagePath}; ./rar-extractor.sh '{$this->oldName}' '{$this->newName}'";
+        $process = new Process([$command]);
+        $process->run();
+        if (!$process->isSuccessful())
+            throw new ProcessFailedException($process);
     }
 }
