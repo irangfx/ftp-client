a49271e9d95d1e59a67ca55230fb8e7679c68c95
HamidNE <hamidne@mail.ru>
2019-06-01 18:16:38 +0430

    edit for rar-extractor.sh command

--- rar-extractor.sh
+++ rar-extractor.sh
@@ -1,17 +1,19 @@
+#!/usr/bin/env bash
+
 oldPassword='tarhan.ir';
 newPassword='irangfx.com';
 
-extract () (
+extract () { (
   set -e
   archive=$1
   case "$archive" in
     -) :;; # read from stdin
     /*) :;; # already an absolute path
-    *) archive=$PWD/$archive;; # make absolute path
+    *) archive=$PWD/${archive};; # make absolute path
   esac
   target=$2
   program=$3
-  if [ -e "$target" ]; then
+  if [[ -e "$target" ]]; then
     echo >&2 "Target $target already exists, aborting."
     return 3
   fi
@@ -21,25 +23,25 @@ extract () (
     *) parent=$PWD;;
   esac
   temp=$(TMPDIR="$parent" mktemp -d)
-  (cd "$temp" && $program "$archive")
+  (cd "$temp" && ${program} "$archive")
   root=
   for member in "$temp/"* "$temp/".*; do
     case "$member" in */.|*/..) continue;; esac
-    if [ -n "$root" ] || ! [ -d "$member" ]; then
+    if [[ -n "$root" ]] || ! [[ -d "$member" ]]; then
       root=$temp # There are multiple files or there is a non-directory
       break
     fi
     root="$member"
   done
-  if [ -z "$root" ]; then
+  if [[ -z "$root" ]]; then
     # Empty archive
-    root=$temp
+    root=${temp}
   fi
   mv -v -- "$root" "$target"
-  if [ "$root" != "$temp" ]; then
+  if [[ "$root" != "$temp" ]]; then
     rmdir "$temp"
   fi
-)
+) }
 
 process () {
   dir=${1%.*}
@@ -48,7 +50,7 @@ process () {
     *.zip|*.ZIP) program="unzip -P $oldPassword";;
     *) echo >&2 "$0: $1: unsupported archive type"; exit 4;;
   esac
-  if [ -d "$dir" ]; then
+  if [[ -d "$dir" ]]; then
     echo >&2 "$0: $dir: directory already exists"
     exit 1
   fi
@@ -60,9 +62,9 @@ newDir=${2%.*}
 mv $1 $2
 process "$2";
 rm $2
-cd $newDir
+cd ${newDir}
 sed 's+www.tarhan.ir/academy+irangfx.com+g' Tarhan.ir.url > IranGFX.com.url
 rm Tarhan.ir.url
 cd ..
-rar -p$newPassword a -r -rr5 $2 ./$newDir
-rm -rf $newDir
+rar -p${newPassword} a -r -rr5 $2 ./${newDir}
+rm -rf ${newDir}
