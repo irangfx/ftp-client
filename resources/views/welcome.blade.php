2ce34fbf92852f0cb31b48f46a9b0689b7b90261
HamidNE <hamidne@mail.ru>
2019-06-06 10:17:02 +0430

    fixed layout bug

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,8 +12,8 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-<div class="container">
-    <div class="d-flex align-items-center justify-content-center">
+<div class="container" style="height: 100vh;">
+    <div class="d-flex align-items-center justify-content-center h-100">
         <form method="POST" action="{{ route('download') }}" class="w-75">
             @csrf
             <div class="form-group">
